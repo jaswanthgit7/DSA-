297fc50280d2341629884fe1c59216bd7249625d
jaswanthgit7 <156202676+jaswanthgit7@users.noreply.github.com>
2025-10-30 20:22:51 +0530

    Time: 2 ms (84.37%) | Memory: 46.1 MB (13.61%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+    int n=nums.length;
+    int res= 0;
+    for(int i=0;i<n;i ++) {
+        if(i>res)
+        return false;
+        res=Math.max(res,i+nums[i]);
+       } 
+       return true;
+    }
+}
