64d34d1b68283454a4712d8a6b0af08151f105d9
jaswanthgit7 <156202676+jaswanthgit7@users.noreply.github.com>
2026-05-25 12:01:07 +0530

    Time: 5 ms (100.00%) | Memory: 47.1 MB (100.00%) - LeetSync

--- 4313-password-strength/password-strength.java
+++ 4313-password-strength/password-strength.java
@@ -1,17 +1,29 @@
 class Solution {
     public int passwordStrength(String password) {
-        int point=0;
-        HashSet<Character> set=new HashSet<>();
-        for(char ch:password.toCharArray()){
-            if(set.contains(ch)) continue;
-            else{
-                set.add(ch);
-                if(ch>='a' && ch<='z')point+=1;
-                else if(ch>='A' && ch<='Z')point+=2;
-                else if(ch>='0' && ch<='9')point+=3;
-                else point+=5;
+        boolean[] seen = new boolean[128];
+        char[] cha = password.toCharArray();
+        int sum = 0;
+        for(char ch : cha){
+            if(!seen[ch]){
+                sum += points(ch);
+                seen[ch] = true;
             }
         }
-        return point;
+        return sum;
+    }
+    static int points(char a){
+        if( Character.isLowerCase(a)){
+            return 1;
+        }
+        if(Character.isUpperCase(a)){
+            return 2;
+        }
+        if(Character.isDigit(a)){
+            return 3;
+        }
+        if("!@#$".contains(String.valueOf(a))) {
+            return 5;
+        }
+        return 0;
     }
 }
