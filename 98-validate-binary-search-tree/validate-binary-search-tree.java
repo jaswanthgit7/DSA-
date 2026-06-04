e563a70b7574cbef75bc6d01e98ee200e09b30a7
jaswanthgit7 <156202676+jaswanthgit7@users.noreply.github.com>
2026-06-04 17:20:35 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (48.60%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -13,24 +13,14 @@
  *     }
  * }
  */
-class Solution {
+public class Solution {
     public boolean isValidBST(TreeNode root) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        inorder(root, arr);
-
-        for (int i = 1; i < arr.size(); i++) {
-            if (arr.get(i) <= arr.get(i - 1)) {
-                return false;
-            }
-        }
-        return true;
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
-
-    private void inorder(TreeNode node, ArrayList<Integer> arr) {
-        if (node == null) return;
-
-        inorder(node.left, arr);
-        arr.add(node.val);
-        inorder(node.right, arr);
+    
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
     }
 }
