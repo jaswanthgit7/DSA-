1a52d684b382422d6c940e2debff564d603f4904
jaswanthgit7 <156202676+jaswanthgit7@users.noreply.github.com>
2026-07-21 16:44:28 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (65.67%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,7 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-        double power=Math.pow(x,n);
-        return (double) power;
-        
+      long N = n;
+      if(N < 0){
+        x=1/x;
+        N = -N;
+      }
+      double result = 1.0;
+      double currp = x;
+        while(N > 0){
+            if(N%2 == 1){
+                result = result*currp ;
+            }
+            currp *= currp;
+            N/=2;
+        }
+        return result;
     }
 }
